d4fb209f38c0c6015c09b2408c8dc353d0684611
Gerson Vásquez <gerson.vasquez23@itca.edu.sv>
2024-10-06 20:58:54 -0600

    modelos
    creacion de modelo ComprasItem

---
+++ app/Models/ComprasItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComprasItem extends Model
+{
+    protected $fillable = [
+        'compra_id',
+        'producto_id',
+        'cantidad',
+        'descuento',
+        'precio_unitario',
+        'total'
+
+    ];
+    use HasFactory;
+
+    public function compra()
+    {
+        return $this->belongsTo(Compra::class, 'compra_id');
+    }
+    public function Producto()
+    {
+        return $this->belongsTo(Producto::class, 'producto_id');
+    }
+}

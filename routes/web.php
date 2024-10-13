8afbaeb527ae5fd1155223cb727c3b5578cb6610
AndyMorales04 <andy.morales23@itca.edu.sv>
2024-10-13 14:12:02 -0600

    Creación de CRUD de marca

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use App\Http\Controllers\ImpuestosController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarcaController;
 
 //Rutas para impuestos
 Route::get('/impuestos', [ImpuestosController::class, 'index'])->name('impuestos.index');
 Route::post('/impuestos', [ImpuestosController::class, 'store'])->name('impuestos.store');
 Route::put('/impuestos/{id}', [ImpuestosController::class, 'update'])->name('impuestos.update');
 Route::delete('/impuestos/{id}', [ImpuestosController::class, 'destroy'])->name('impuestos.destroy');
+
+//Rutas de marca
+Route::get('/marcas', [MarcaController::class, 'index'])->name('marcas.index');
+Route::post('/marcas', [MarcaController::class, 'store'])->name('marcas.store');
+Route::put('/marcas/{id}', [MarcaController::class, 'update'])->name('marcas.update');
+Route::delete('/marcas/{id}', [MarcaController::class, 'destroy'])->name('marcas.destroy');

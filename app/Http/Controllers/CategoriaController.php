ec80c10b96fefe75f1014118e193581af92256d7
AndyMorales04 <andy.morales23@itca.edu.sv>
2024-10-13 14:14:51 -0600

    Creación del CRUD de Categoria

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    //Mostrar 
+    public function index()
+    {
+        $categorias = Categoria::paginate(10);
+        return view('categorias.index', compact('categorias'));
+    }
+
+    //Crear
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'nullable|string|max:1000',
+        ]);
+
+        Categoria::create($request->all());
+
+        return redirect()->route('categorias.index')->with('success', 'Categoría creada exitosamente.');
+    }
+
+    //Actualizar
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'nullable|string|max:1000',
+        ]);
+
+        $categoria = Categoria::findOrFail($id);
+        $categoria->update($request->all());
+
+        return redirect()->route('categorias.index')->with('success', 'Categoría actualizada exitosamente.');
+    }
+
+    //Eliminar
+    public function destroy($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->delete();
+
+        return redirect()->route('categorias.index')->with('success', 'Categoría eliminada exitosamente.');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ImpuestosController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\CategoriaController;
 
 //Rutas para impuestos
 Route::get('/impuestos', [ImpuestosController::class, 'index'])->name('impuestos.index');
@@ -15,3 +16,9 @@ Route::get('/marcas', [MarcaController::class, 'index'])->name('marcas.index');
 Route::post('/marcas', [MarcaController::class, 'store'])->name('marcas.store');
 Route::put('/marcas/{id}', [MarcaController::class, 'update'])->name('marcas.update');
 Route::delete('/marcas/{id}', [MarcaController::class, 'destroy'])->name('marcas.destroy');
+
+//Rutas de categoria
+Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
+Route::put('/categorias/{id}', [CategoriaController::class, 'update'])->name('categorias.update');
+Route::delete('/categorias/{id}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');

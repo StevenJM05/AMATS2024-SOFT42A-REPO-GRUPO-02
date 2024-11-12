b6814a7f5b853d8506bc8059b26c890b058d6bb1
Cxrloxs17 <antonio.sandoval22@itca.edu.sv>
2024-11-12 10:41:20 -0600

    Controlador de Dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Compra;
+use App\Models\Venta;
+use App\Models\Producto;
+use App\Models\User;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $totalVentas = Venta::sum('total');
+        $totalCompras = Compra::sum('total');
+        $productosCount = Producto::count();
+        $usuariosCount = User::count();
+        $productosRecientes = Producto::latest()->take(5)->get();
+
+        return view('dashboard.index', compact('totalVentas', 'totalCompras', 'productosCount', 'usuariosCount', 'productosRecientes'));
+    }
+}
